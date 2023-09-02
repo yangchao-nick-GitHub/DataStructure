198eb88019a14eb8e9d9a5fa0c548bcd2eeb87b5
yangchao <yangchao_nick@163.com>
2023-09-02 16:49:13 +0800

    feat:完善ArrayStack代码

--- arraystack/array_stack.h
+++ arraystack/array_stack.h
@@ -3,6 +3,8 @@
 #define H_ARRAY_STACK_H
 
 #include <stdlib.h>
+#include <assert.h>
+#include <iostream>
 #define internal_allocate malloc
 #define internal_deallocate free
 
@@ -15,7 +17,7 @@ typedef struct internal_hook {
 template<class T>
 class ArrayStack {
 public:
-    ArrayStack(int size);
+    ArrayStack(int size = 10);
     ~ArrayStack();
     void StackPush(T data);
     T StackPop();
@@ -30,7 +32,82 @@ private:
     int m_size;
 };
 
+template<class T>
+internal_hook ArrayStack<T>::m_hook = { internal_allocate,  internal_deallocate};
 
+
+/*
+    1. 默认参数在声明时指定
+    2. 类模板构造析构函数实现放在头文件中
+*/
+template<class T>
+ArrayStack<T>::ArrayStack(int size) 
+    :m_item(nullptr), m_capacity(size), m_size(0) 
+{
+    m_item = new T[size];
+    if (nullptr == m_item) {
+        std::cout << "allocate error\n";
+    }
+}
+
+template<class T>
+ArrayStack<T>::~ArrayStack()
+{
+    if (m_item) {
+        delete[] m_item;
+        m_item = nullptr;
+    }
+    
+}
+
+template<class T>
+void ArrayStack<T>::StackPush(T data)
+{
+    assert(m_item);
+    if (m_size >= m_capacity) {
+        std::cout<< "stack is full!\n";
+        return ;
+    }
+    m_item[m_size++] = data;
+}
+
+template<class T>
+bool ArrayStack<T>::IsEmpty()
+{
+    return m_size == 0;
+}
+
+template<class T>
+T ArrayStack<T>::StackPop()
+{
+    assert(m_item);
+    if (m_size <= 0) {
+        std::cout << "stack is empty\n";
+        return 0;
+    }
+    return m_item[--m_size]; 
+}
+
+template<class T>
+T ArrayStack<T>::StackTop()
+{
+    assert(m_item);
+    if (m_size <= 0) {
+        std::cout << "stack is empty\n";
+        return 0;
+    }
+    return m_item[m_size - 1]; 
+}
+
+template<class T>
+T ArrayStack<T>::GetPosStackItme(int pos)
+{
+    assert(m_item);
+    if (pos < 0 || pos > m_capacity) {
+        return 0;
+    }
+    return m_item[pos - 1];
+}
 
 #endif
 
--- arraystack/test.cpp
+++ arraystack/test.cpp
@@ -6,6 +6,26 @@ using namespace std;
 
 int main()
 {
-    ArrayStack<int> stack(10);
+    ArrayStack <int> stack(10);
+    // stack.StackPush(1);
+    // stack.StackPush(2);
+    // stack.StackPush(3);
+    // std::cout << stack.IsEmpty() << std::endl;
+    // std::cout << stack.StackTop() << std::endl;
+    // std::cout << stack.StackPop() << std::endl;
+    // std::cout << stack.StackPop() << std::endl;
+    // std::cout << stack.StackPop() << std::endl;
+    // std::cout << stack.IsEmpty() << std::endl;
+
+    // stack.StackPush(1);
+    // stack.StackPush(2);
+    // stack.StackPush(3);
+    // stack.StackPop();
+    // stack.StackPop();
+    // std::cout << stack.StackTop() << std::endl;
+
+
+
+
     return 0;
 }
