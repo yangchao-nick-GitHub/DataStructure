033c3e5b7447e4c5ce5e13951dcd1be6aad24f47
yangchao <yangchao_nick@163.com>
2023-09-03 15:59:01 +0800

    完善LinkStack代码

--- linkstack/link_stack.h
+++ linkstack/link_stack.h
@@ -3,39 +3,49 @@
 
 #include <stdlib.h>
 #include <iostream>
-template<class T>
-struct StackNode {
-public:
-    StackNode(T data):m_data(data), m_next(nullptr) {};
-    T m_data;
-    struct StackNode<T> *m_next;
-};
 
-template<class T>
+typedef void* DATATYPE;
+
+typedef struct StackNode {
+public:
+    StackNode():m_data(nullptr), m_next(nullptr) {};
+    StackNode(DATATYPE data):m_data(data), m_next(nullptr) {};
+    DATATYPE m_data;
+    StackNode *m_next;
+    
+}StackNode;
+
 class LinkStack {
 public:
     LinkStack();
     ~LinkStack();
+    bool IsEmpty();
+    size_t StackSize();
+    void StackPush(DATATYPE data);
+    DATATYPE StackPop();
+    DATATYPE StackTop();
+    DATATYPE GetPosStackItem(size_t pos);
+
 private:
-    struct StackNode<T> *m_head;
-    int m_size;
+    StackNode* AllocateNode(DATATYPE data);
+    struct StackNode *m_head;
+    size_t m_size;
 };
 
-template<class T>
-LinkStack<T>::LinkStack()
+LinkStack::LinkStack()
 {
-    m_head = new struct StackNode<T>;
+    m_head = AllocateNode(nullptr);
     if (nullptr == m_head) {
-        std::cout << "LinkStack error!";
+        std::cout << "LinkStack error" << std::endl;
+        return ;
     }
     m_size = 0;
 }
 
-template<class T>
-LinkStack<T>::~LinkStack()
+LinkStack::~LinkStack()
 {
-    struct StackNode<T> *pre = NULL;
-    struct StackNode<T> *cur = NULL;
+    struct StackNode *pre = NULL;
+    struct StackNode *cur = NULL;
     if (nullptr == m_head) {
         return ;
     }
@@ -48,6 +58,93 @@ LinkStack<T>::~LinkStack()
     }
 
     free(m_head);
+}
+
+size_t LinkStack::StackSize()
+{
+    return m_size;
+}
+
+bool LinkStack::IsEmpty()
+{
+    return m_size == 0;
+}
+
+StackNode* LinkStack::AllocateNode(DATATYPE data)
+{
+    StackNode *node = new StackNode();
+    if (nullptr == node) {
+        std::cout << "AllocateNode error" << std::endl;
+        return nullptr;
+    }
+    node->m_data = data;
+    node->m_next = nullptr;
+    return node;
+}
+
+void LinkStack::StackPush(DATATYPE data) // 前插法
+{
+    if (nullptr == m_head) {
+        return ;
+    }
+    StackNode *node = AllocateNode(data);
+    if (nullptr == node) {
+        std::cout << "StackPush error" << std::endl; 
+        return ;
+    }
+    node->m_next = m_head->m_next;
+    m_head->m_next = node;
+    m_size++;
+}
+
+DATATYPE LinkStack::StackPop()
+{
+    if (nullptr == m_head) {
+        std::cout << "StackPop error" << std::endl;
+        return nullptr;
+    }
+    StackNode *top = m_head->m_next;
+    if (nullptr == top) {
+        return nullptr;
+    }
+    m_head->m_next = top->m_next;
+    DATATYPE data = top->m_data;
+    free(top);
+    top = nullptr;
+    m_size--;
+    return data;
+}
+
+DATATYPE LinkStack::StackTop()
+{
+    if (nullptr == m_head) {
+        std::cout << "StackTop error" << std::endl;
+        return nullptr;
+    }
+    return m_head->m_next->m_data;
+}
+
+DATATYPE LinkStack::GetPosStackItem(size_t pos) // pos:1 - size;
+{
+    if (nullptr == m_head) {
+        return nullptr;
+    }
+    if (pos < 1 || pos > m_size) {
+        return nullptr;
+    }
+    size_t tmp_pos = 0;
+    StackNode *cur = m_head;
+    while (cur != NULL) {
+        cur = cur->m_next;
+        tmp_pos++;
+        if (tmp_pos == pos) {
+            break;
+        }
+    }
+    if (tmp_pos != pos) {
+        return nullptr;
+    }
+    return cur->m_data;
 }
 
 #endif
--- linkstack/test.cpp
+++ linkstack/test.cpp
@@ -3,7 +3,21 @@
 
 int main()
 {
-    LinkStack<int> stack;
+    const char* arr[] = {"123", "456", "789"};
+    LinkStack stack;
+    stack.StackPush((void*)arr[0]);
+    stack.StackPush((void*)arr[1]);
+    stack.StackPush((void*)arr[2]);
+    char *str = (char*)stack.GetPosStackItem(3);
+    std::cout << str << std::endl;
+    std::cout << stack.StackSize() << std::endl;
+    std::cout << stack.IsEmpty() << std::endl;
+    stack.StackPop();
+    stack.StackPop();
+    stack.StackPop();
+    std::cout << stack.StackSize() << std::endl;
+    std::cout << stack.IsEmpty() << std::endl;
+
     return 0;
 }
 
